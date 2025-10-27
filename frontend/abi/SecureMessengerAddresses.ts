2e7c433b161b0ae45410c42d60ce42859cfcdd8f
planetsearcher <planetsearcher@proton.me>
2025-10-27 19:13:14 +0100

    Add SecureMessengerAddresses.ts

---
+++ frontend/abi/SecureMessengerAddresses.ts
@@ -0,0 +1,9 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+*/
+export const SecureMessengerAddresses = {
+  "11155111": { address: "0x138C26aF0dCe90C86e73DdAe3a4705242aa1407b", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x83902109B6164eB1c75C885A42DeDEE251f60DB9", chainId: 31337, chainName: "hardhat" },
+};
