2a1fb9ec70e5b372d3f1a26f4fde12b4c355ec39
planetsearcher <planetsearcher@proton.me>
2025-10-27 19:13:10 +0100

    Add EncryptedLedgerAddresses.ts

---
+++ frontend/abi/EncryptedLedgerAddresses.ts
@@ -0,0 +1,9 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+*/
+export const EncryptedLedgerAddresses = {
+  "11155111": { address: "0x83902109B6164eB1c75C885A42DeDEE251f60DB9", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x62a74B198C6D1080CA6d5a84d0b77D43e62C6fC1", chainId: 31337, chainName: "hardhat" },
+};
