8c8be3f63860e80e24d1c772a559156f995059b5
planetsearcher <planetsearcher@proton.me>
2025-10-27 19:13:12 +0100

    Add FHECounterAddresses.ts

---
+++ frontend/abi/FHECounterAddresses.ts
@@ -0,0 +1,9 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+*/
+export const FHECounterAddresses = {
+  "11155111": { address: "0xE27f04942A2cE9380329B7e2b1456336e5378EbC", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0xE27f04942A2cE9380329B7e2b1456336e5378EbC", chainId: 31337, chainName: "hardhat" },
+};
